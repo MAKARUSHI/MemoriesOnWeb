704ed92b2266c03be5da1983aa422fd3b4346d47
MAKARUSHI <130999372+MAKARUSHI@users.noreply.github.com>
2024-05-03 22:04:37 +0200

    Add files via upload

--- JS-CODE/memoriesImgs.js
+++ JS-CODE/memoriesImgs.js
@@ -7,11 +7,11 @@ const memoriesImgs = [
 ];
 
 const memoriesImgs2 = [
-    './IMGS/MEMORIES-IMGS/MEMIMG0000.jpg',
-    './IMGS/MEMORIES-IMGS/MEMIMG0002.jpg',
-    './IMGS/MEMORIES-IMGS/MEMIMG0001.jpg',
-    './IMGS/MEMORIES-IMGS/MEMIMG0003.jpg',
-    './IMGS/MEMORIES-IMGS/MEMIMG0004.jpg'
+    'IMGS/MEMORIES-IMGS/MEMIMG0000.jpg',
+    'IMGS/MEMORIES-IMGS/MEMIMG0002.jpg',
+    'IMGS/MEMORIES-IMGS/MEMIMG0001.jpg',
+    'IMGS/MEMORIES-IMGS/MEMIMG0003.jpg',
+    'IMGS/MEMORIES-IMGS/MEMIMG0004.jpg'
 ];
 
 export default [memoriesImgs , memoriesImgs2];
