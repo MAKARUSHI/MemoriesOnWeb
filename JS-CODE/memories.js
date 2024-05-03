a0a5b4ab142c9ce01c0bb73353a1699d3a335d3f
MAKARUSHI <130999372+MAKARUSHI@users.noreply.github.com>
2024-05-03 20:59:33 +0200

    Add files via upload

---
+++ JS-CODE/memories.js
@@ -0,0 +1,13 @@
+const memories = [{
+    title: "First Meet",
+    content: "vs bmk bdsjvbnsdhsnjvdfkdklmkfkj dgj rfgkjdfjkhjgkerj gmnrkojgknr em,gnkjrj ngkdkgnje klćgjijkwemg kr dklgn rmkngjkslbvsdklvn dsvnkl vnv dsv n",
+    memImg: `../IMGS/MEMORIES-IMGS/MEMIMG0002.jpg`,
+    memVideo: `../IMGS/MEMORIES-VIDEOS/MEMVIDEO0006.mp4`
+}, {
+    title: "Second Meet",
+    content: "kl vnvvrfdlgkn rmgkrnjkgjkojsrdjngflopvrejibv m grmjk jgkf gmrmkjkgbkskerlkgv nroigf rgknrklmg rkjmmg s, m rvfmkrs mgrkmmfgkr nkmgfmk lrnokfmmrs mfrmlksnnfblers,mmfkb  rklg mglkmrsjkčfgnklčrjkg b,m dsv n",
+    memImg: `../IMGS/MEMORIES-IMGS/MEMIMG0004.jpg`,
+    memVideo: ""
+}];
+
+export default memories;
